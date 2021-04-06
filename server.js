7124834079a989bc2c561ce9ec8d92e968c6b035
Manuela Carvalho <mcalmeida.13@gmail.com>
2021-04-06 15:20:57 -0300

    arquivo de texto

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import * as http from 'http';
+import { createReadStream, createWriteStream } from 'fs';
+
 
 const server = http.createServer((req,res) =>{
 
-    // if(req.url === '/'){
-    //     res.write('Hello world from nodejs');
-    //     res.end();
-    // } else {
-    //     res.write('Wrong way buddy');
-    //     res.end();   
-    // }
+    const readStream  = createReadStream('./index.html');
+    res.writeHead(200,{'Content-type': 'text/html'});
+    readStream.pipe(res);
+
 });    
 
 server.listen('3000');
